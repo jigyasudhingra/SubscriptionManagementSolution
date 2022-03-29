21eb80ebe1c799927d1e98c016e4af9a2f4967f2
Jigyasu Dhingra <68184200+jigyasudhingra@users.noreply.github.com>
2022-03-29 07:31:28 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c0cb27445c8a7e708d949e80e66c3aff.js"
+  "/SubscriptionManagementSolution/precache-manifest.3faeaa81e30ef8ed00ece8e457b6f8b0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/SubscriptionManagementSolution/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
