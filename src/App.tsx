a883b3ff7d96a2c24bd61871824b30de2ce6730f
Jigyasu Dhingra <68184200+jigyasudhingra@users.noreply.github.com>
2022-03-29 07:45:43 +0530

    Add HashRouter instead of BrowserRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 import { MuiThemeProvider } from '@material-ui/core/styles';
 import Theme from './Theme';
 import RootLayout from './Screens/RootLayout';
@@ -8,11 +8,11 @@ import Boot from './Boot';
 const App: React.FC = () => {
   Boot().then(() => { });
   return (
-    <BrowserRouter>
+    <HashRouter>
       <MuiThemeProvider theme={Theme}>
         <RootLayout />
       </MuiThemeProvider>
-    </BrowserRouter>
+    </HashRouter>
   );
 };
 
